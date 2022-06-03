42020af5e2191e28cf5d50c6450e7f225137eaf2
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-06-03 23:33:34 +0000

    in progro

---
+++ data.js
@@ -0,0 +1,34 @@
+var data = {
+  view: 'sunday',
+  sunday: [{
+    time: null,
+    stuff: null
+  }],
+  monday: [{
+    time: null,
+    stuff: null
+  }],
+  tuesday: [{
+    time: null,
+    stuff: null
+  }],
+  wednesday: [{
+    time: null,
+    stuff: null
+  }],
+  thursday: [{
+    time: null,
+    stuff: null
+  }],
+  friday: [{
+    time: null,
+    stuff: null
+  }],
+  saturday: [{
+    time: null,
+    stuff: null
+  }],
+
+  editing: null,
+  nextEntryID: 1
+};
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var $entryButt = document.querySelector('.add-button');
 var $modal = document.querySelector('.modal');
 var $exitButt = document.querySelector('.exit-button');
 var $days = document.querySelectorAll('.day');
+var $tbl = document.querySelector('table');
+var $form = document.querySelector('form');
 
 function addEntry(event) {
   $modal.className = 'modal';
@@ -16,9 +18,31 @@ function exitModal(event) {
 $exitButt.addEventListener('click', exitModal);
 
 function days(event) {
-  for (var i = 0; i < $days.length; i++) {
+  // for (var i = 0; i < $days.length; i++) {
 
-  }
+  // }
 }
 
-$days.addEventListener('click', days);
+function renderTbl(event) {
+  var $tr = document.createElement('tr');
+
+  var $td1 = document.createElement('td');
+  $td1.textContent = data.task[event].time[event];
+  $tr.appendChild($td1);
+
+  var $td2 = document.createElement('td');
+  $td2.textContent = '';
+  $tr.appendChild($td2);
+
+  return $tr;
+}
+
+// $days.addEventListener('click', days);
+
+// function
+
+$form.addEventListener('submit', addEntry);
+
+function addEntry(event) {
+
+}
