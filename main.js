b9952004ebb344da80c988ab3591ff4259e69521
Andy Chen <chen.andy18@gmail.com>
2022-06-03 20:32:56 +0000

    lunch break

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-var entryBut = document.querySelector('.add-button');
+var $entryButt = document.querySelector('.add-button');
 var $modal = document.querySelector('.modal');
+var $exitButt = document.querySelector('.exit-button');
+var $days = document.querySelectorAll('.day');
 
 function addEntry(event) {
   $modal.className = 'modal';
 }
 
-entryBut.addEventListener('click', addEntry);
+$entryButt.addEventListener('click', addEntry);
+
+function exitModal(event) {
+  $modal.className = 'modal hidden';
+}
+
+$exitButt.addEventListener('click', exitModal);
+
+function days(event) {
+  for (var i = 0; i < $days.length; i++) {
+
+  }
+}
+
+$days.addEventListener('click', days);
