209a7b7c842ee34d8fa88ac59bd090da4fcff9af
Nicholas Fernandez <Nickfer610@gmail.com>
2022-06-03 20:06:07 +0000

    more progress

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var entryBut = document.querySelector('.add-button');
+var $modal = document.querySelector('.modal');
 
 function addEntry(event) {
-
+  $modal.className = 'modal';
 }
 
 entryBut.addEventListener('click', addEntry);
